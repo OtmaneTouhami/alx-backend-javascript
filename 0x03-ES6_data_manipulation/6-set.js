1becc40982361698f0800d10ad7a40f36288c3f7
OtmaneTouhami <otmanetouhami01@gmail.com>
2024-07-30 14:33:22 +0100

    0x03-ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Otmane TOUHAMI <https://github.com/OtmaneTouhami>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
