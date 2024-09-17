c74f1ab81bc44d8fa18185d13504418b9b1687fa
OtmaneTouhami <otmanetouhami01@gmail.com>
2024-09-17 15:34:24 +0100

    0x05-Node_JS_basic

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Otmane Touhami <https://github.com/OtmaneTouhami>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
